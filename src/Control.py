5f5850e93c2a5250057b7cc1b8d5964794666a50
Nanpou-Haku <121656187+Nanpou-Haku@users.noreply.github.com>
2023-07-01 16:33:59 +0900

    Add files via upload
    fix

--- src/Control.py
+++ src/Control.py
@@ -1,4 +1,5 @@
 import time
+import copy
 import atexit
 import picamera
 import picamera.array
@@ -7,7 +8,6 @@ from dataclasses import dataclass
 import cv2
 import numpy as np
 from buildhat import Motor, ForceSensor
-import copy
 
 @dataclass
 class ColorDatas:
@@ -56,11 +56,10 @@ clock = 0
 direction = 0
 counter = 0
 pole = None
-
-startFlag = False;
+startFlag = False
 
 #Hiroshi
-def init():
+def Init():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)              
     GPIO.setup(trig_front, GPIO.OUT)          
@@ -85,12 +84,12 @@ def init():
     camera.framerate = frame
     
 #Hiroshi
-def fini():
+def Fin():
     motor.stop()
     cv2.destroyAllWindows()
 
 #Hiroshi
-def motorMove(motor, nowPosition, goPosition):
+def MotorMove(motor, nowPosition, goPosition):
     global startFlag
     if(nowPosition != goPosition):
         motor.run_to_position(goPosition)
@@ -114,7 +113,7 @@ def motorMove(motor, nowPosition, goPosition):
     return nowPosition
 
 #Hiroshi
-def motorPosition(color, left, center, right):
+def MotorPosition(color, left, center, right):
     cube_x = int(color.moments["m10"] / color.moments["m00"])
     cube_y = int(color.moments["m01"] / color.moments["m00"])
     cv2.circle(color.dstt, (cube_x, cube_y), 2, 100, 2, 4)
@@ -131,9 +130,8 @@ def motorPosition(color, left, center, right):
     return position
 
 #Yasui
-def read_distance_front():
+def Read_distance_front():
     GPIO.output(trig_front, GPIO.HIGH)
-    time.sleep(0.00001)
     GPIO.output(trig_front, GPIO.LOW)
 
     while GPIO.input(echo_front) == GPIO.LOW:
@@ -146,9 +144,8 @@ def read_distance_front():
     return distance_front
 
 #Yasui
-def read_distance_side():
+def Read_distance_side():
     GPIO.output(trig_side, GPIO.HIGH)
-    time.sleep(0.00001)
     GPIO.output(trig_side, GPIO.LOW)
 
     while GPIO.input(echo_side) == GPIO.LOW:
@@ -161,9 +158,9 @@ def read_distance_side():
     return distance_side
 
 #Yasui
-def distanceMove(steering, nowPosition, left):
-    cm_front = read_distance_front()
-    cm_side = read_distance_side()
+def DistanceMove(steering, nowPosition, left):
+    cm_front = Read_distance_front()
+    cm_side = Read_distance_side()
     global pole
     
     if cm_front < 40:
@@ -177,9 +174,9 @@ def distanceMove(steering, nowPosition, left):
             steering.run_to_position(0)
             motor.run_for_seconds(2, -100)
             motor.stop()
-            situation()
+            Situation()
             
-            nowPosition = motorMove(steering, nowPosition, 0)
+            nowPosition = MotorMove(steering, nowPosition, 0)
             
         else:
             direction = 1
@@ -190,9 +187,9 @@ def distanceMove(steering, nowPosition, left):
             steering.run_to_position(0)
             motor.run_for_seconds(2, -100)
             motor.stop()
-            situation()
+            Situation()
             
-            nowPosition = motorMove(steering, nowPosition, 0)
+            nowPosition = MotorMove(steering, nowPosition, 0)
             
     else:
         motor.start(30)
@@ -202,71 +199,7 @@ def distanceMove(steering, nowPosition, left):
     return nowPosition
 
 #Fujimura
-def avoid(nowPosition):
-    if pole == 1 and counter == 1:
-        print("1 : 1")
-    
-    elif pole == 2 and counter == 0:
-        print("2 : 1")
-        
-    elif pole == 2 and coutner == 2:
-        print("2 : 2")
-    
-    else:
-        #緑あり
-        if (len(green.contour) != 0):
-            green.maxContour = max(green.contour, key = lambda x: cv2.contourArea(x))
-            green.area = cv2.contourArea(green.maxContour)
-            green.moments = cv2.moments(green.maxContour)
-            #赤あり
-            if (len(red.contour) != 0):
-                red.maxContour = max(red.contour, key = lambda x: cv2.contourArea(x))
-                red.area = cv2.contourArea(red.maxContour)
-                red.moments = cv2.moments(red.maxContour)
-                #緑が近いなら緑を優先
-                if red.area < green.area and 1300 < green.area < 9000:
-                    if green.moments["m00"] != 0:
-                        goPosition = motorPosition(green, left, left, 0)
-                    nowPosition = motorMove(steering, nowPosition, goPosition)
-            
-                #赤が近いなら赤を優先
-                elif green.area < red.area and 1300 < red.area < 9000:
-                    if red.moments["m00"] != 0:
-                        goPosition = motorPosition(red, 0, right, right)
-                        nowPosition = motorMove(steering, nowPosition, goPosition)
- 
-                else:
-                    nowPosition = distanceMove(steering, nowPosition, left)
-        
-            #緑あり赤なし
-            elif 1300 < green.area < 9000:
-                if green.moments["m00"] != 0:
-                    goPosition = motorPosition(green, left, left, 0)
-                    nowPosition = motorMove(steering, nowPosition, goPosition)
-            
-            else:
-                nowPosition = distanceMove(steering, nowPosition, left)
-    
-        #緑なし赤あり
-        elif (len(red.contour) != 0):
-            red.maxContour = max(red.contour, key = lambda x: cv2.contourArea(x))
-            red.area = cv2.contourArea(red.maxContour)
-            red.moments = cv2.moments(red.maxContour)
-            if 1300 < red.area < 9000:
-                if red.moments["m00"] != 0:
-                    goPosition = motorPosition(red, 0, right, right)
-                    nowPosition = motorMove(steering, nowPosition, goPosition)
-        
-            else:
-                nowPosition =  distanceMove(steering, nowPosition, left)
-    
-        else:
-            nowPosition = distanceMove(steering, nowPosition, left)
-        
-    return nowPosition
-
-#Fujimura
-def back(nowPosition, color):
+def Back(nowPosition, color):
     global total
     global clock
     
@@ -300,19 +233,83 @@ def back(nowPosition, color):
     clock = 0
     
 #Fujimura
-def situation():
+def Situation():
     surf = max([green.area, red.area])
     global pole
 
     if 50 <= surf <=70 or 90 <= surf <= 130:
-        pole = 1 #1本
+        pole = 1
         
     elif 200 <= surf <= 570:
-        pole = 2 #おそらく1,2本
+        pole = 2
+
+#Fujimura
+def Avoid(nowPosition):
+    if pole == 1 and counter == 1:
+        print("1 : 1")
+    
+    elif pole == 2 and counter == 0:
+        print("2 : 1")
+        
+    elif pole == 2 and coutner == 2:
+        print("2 : 2")
+    
+    else:
+        #Green
+        if (len(green.contour) != 0):
+            green.maxContour = max(green.contour, key = lambda x: cv2.contourArea(x))
+            green.area = cv2.contourArea(green.maxContour)
+            green.moments = cv2.moments(green.maxContour)
+            #Green and red
+            if (len(red.contour) != 0):
+                red.maxContour = max(red.contour, key = lambda x: cv2.contourArea(x))
+                red.area = cv2.contourArea(red.maxContour)
+                red.moments = cv2.moments(red.maxContour)
+                #Green near
+                if red.area < green.area and 1300 < green.area < 9000:
+                    if green.moments["m00"] != 0:
+                        goPosition = MotorPosition(green, left, left, 0)
+                    nowPosition = MotorMove(steering, nowPosition, goPosition)
+            
+                #Red near
+                elif green.area < red.area and 1300 < red.area < 9000:
+                    if red.moments["m00"] != 0:
+                        goPosition = MotorPosition(red, 0, right, right)
+                        nowPosition = MotorMove(steering, nowPosition, goPosition)
+ 
+                else:
+                    nowPosition = DistanceMove(steering, nowPosition, left)
+        
+            #Green only
+            elif 1300 < green.area < 9000:
+                if green.moments["m00"] != 0:
+                    goPosition = MotorPosition(green, left, left, 0)
+                    nowPosition = MotorMove(steering, nowPosition, goPosition)
+            
+            else:
+                nowPosition = DistanceMove(steering, nowPosition, left)
+    
+        #Red only
+        elif (len(red.contour) != 0):
+            red.maxContour = max(red.contour, key = lambda x: cv2.contourArea(x))
+            red.area = cv2.contourArea(red.maxContour)
+            red.moments = cv2.moments(red.maxContour)
+            if 1300 < red.area < 9000:
+                if red.moments["m00"] != 0:
+                    goPosition = MotorPosition(red, 0, right, right)
+                    nowPosition = MotorMove(steering, nowPosition, goPosition)
+        
+            else:
+                nowPosition =  DistanceMove(steering, nowPosition, left)
+    
+        else:
+            nowPosition = DistanceMove(steering, nowPosition, left)
+        
+    return nowPosition
 
 #Fujimura
 def main():
-    nowPosition = motorMove(steering, -1, 0)
+    nowPosition = MotorMove(steering, -1, 0)
     stream = picamera.array.PiRGBArray(camera)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
     motor.start(30)
@@ -322,34 +319,34 @@ def main():
         camera.capture(stream, 'bgr', use_video_port=True)
         hsv = cv2.cvtColor(stream.array, cv2.COLOR_BGR2HSV)
         
-        #マスク処理
+        #masking
         green.mask = cv2.inRange(hsv, green.lower, green.upper)
         red.mask = cv2.inRange(hsv, red.lower, red.upper)
         red2.mask = cv2.inRange(hsv, red2.lower, red2.upper)
         red.mask = red.mask + red2.mask
         
-        #オープニング
+        #opening
         green.dst = cv2.erode(green.mask, kernel, iterations=1)
         green.dstt = cv2.dilate(green.dst, kernel, iterations=1)
         red.dst = cv2.erode(red.mask, kernel, iterations=1)
         red.dstt = cv2.dilate(red.dst, kernel, iterations=1)
         
-        #合成
+        #synthesis
         green.res = cv2.bitwise_and(stream.array, stream.array, mask=green.dstt)
         red.res = cv2.bitwise_and(stream.array, stream.array, mask=red.dstt)
         
-        #輪郭検出
+        #outline
         green.contour, green.hierarchy = cv2.findContours(green.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         red.contour, red.hierarchy = cv2.findContours(red.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
-        #分割線描画
+        #split
         cv2.line(green.res, (41, 0), (41,240), (255, 215, 0), 1)
         cv2.line(green.res, (280, 0), (280,240), (255, 215, 0), 1)
         cv2.line(red.res, (41, 0), (41,240), (255, 215, 0), 1)
         cv2.line(red.res, (280, 0), (280,240), (255, 215, 0), 1)
         
         if total == 0:
-            nowPosition = avoid(nowPosition)
+            nowPosition = Avoid(nowPosition)
             
         elif total >= 0.5:
             global clock
@@ -363,9 +360,9 @@ def main():
                 print("red")
                 clock = 2.5
 
-            back(nowPosition, color)
+            Back(nowPosition, color)
         
-        #表示
+        #view
         cv2.imshow("frame", stream.array)
         cv2.imshow("Green", green.res)
         cv2.imshow("Red", red.res)
@@ -374,10 +371,10 @@ def main():
         stream.truncate()
             
 if __name__ == '__main__':
-    init()
+    Init()
 
     try:
         main()
 
     finally:
-        fini()
+        Fin()
